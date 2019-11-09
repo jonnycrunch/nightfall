3c58234f443ec63db4d968ef0c4d49970616468d
vishnu.ps <vishnu.ps@gds.ey.com>
2019-11-09 14:49:06 +0530

    updated shell script for genrating trusted setup

--- install.sh
+++ install.sh
@@ -1,36 +1,5 @@
 #!/usr/bin/env bash
 
-# Getting prrof names based on the user inputs
-declare selectedProof
-proofName(){
-    while :
-    do
-        case $1 in
-            1 ) 
-                selectedProof='ft-mint';
-                break;;
-            2 ) 
-                selectedProof='nft-mint';
-                break;;
-            3 ) 
-                selectedProof='ft-transfer';
-                break;;
-            4 ) 
-                selectedProof='nft-transfer';
-                break;;  
-            5 ) 
-                selectedProof='ft-burn';
-                break;;  
-            6 ) 
-                selectedProof='nft-burn'
-                break;;                                      
-            * ) 
-                echo 'Please type a valid proof number.'
-                exit;;
-        esac
-    done
-} 
-
 # Creating specific proofs
 proofCreation(){
     arg1=$1
@@ -38,31 +7,51 @@ proofCreation(){
     join=' && '
     cmd=''
     inputs=($(echo "$arg1" | tr ',' '\n'))
-    for u in "${inputs[@]}"
-    do
-        proofName $u
-        cmd+=$script$selectedProof$join
+    trustedSetups=()
+
+    for d in *; do
+    if [ -d "$d" ]; then
+        trustedSetups+=($d)
+    fi
     done
+
+    for i in "${inputs[@]}"
+    do
+        cmd+=$script${trustedSetups[`expr $i - 1`]}$join
+    done
+
     cmd+='cd ../';
     eval $cmd
 }
+
 
 # Exit script as soon as a command fails.
 set -o errexit
 
 cd zkp-utils && npm ci && \
+
 cd ../zkp && npm ci && \
+
+cd zkp/code/gm17/
+
+setups=()
+
+for d in *; do
+  if [ -d "$d" ]; then
+    setups+=($d)
+  fi
+done
 
 # Lists of all existing proofs
 echo -e '\033[32mList of completed trusted setups :\033[m'
-setups=()
-[[ -f code/gm17/ft-mint/proving.key && -f code/gm17/ft-mint/verification.key ]] && setups+=('ft-mint')
-[[ -f code/gm17/nft-mint/proving.key && -f code/gm17/nft-mint/verification.key ]] && setups+=('nft-mint')
-[[ -f code/gm17/ft-transfer/proving.key && -f code/gm17/ft-transfer/verification.key ]] && setups+=('ft-transfer')
-[[ -f code/gm17/nft-transfer/proving.key && -f code/gm17/nft-transfer/verification.key ]] && setups+=('nft-transfer')
-[[ -f code/gm17/ft-burn/proving.key && -f code/gm17/ft-burn/verification.key ]] && setups+=('ft-burn')
-[[ -f code/gm17/nft-burn/proving.key && -f code/gm17/nft-burn/verification.key ]] && setups+=('nft-burn')
+existingSetups=()
+
 for i in "${setups[@]}"
+do
+    [[ -f $i/proving.key && -f $i/verification.key ]] && existingSetups+=($i)
+done
+
+for i in "${existingSetups[@]}"
 do
     echo $i
 done
@@ -72,16 +61,24 @@ while true; do
     then
         yn=$1
     else
-        read -p "Do you want to recreate all trusted setups? (y/n) : " yn    
+        read -p $'\n'"Do you want to rerun all trusted setups? (y/n) : " yn    
     fi
     case $yn in
         [Yy]* ) npm run setup-all && cd ../; break;;
-        [Nn]* ) echo -e '\033[32mList of all trusted steups :\033[m'
-                echo -e '1.ft-mint\n2.nft-mint\n3.ft-transfer\n4.nft-transfer\n5.ft-burn\n6.nft-burn'
-                echo -e 'Type the number of each trusted setup seperated by a comma'
-                read -p '(use "1,2" to create trusted setup for ft-mint & nft-mint) : ' proofs
+        [Nn]* ) echo -e '\033[32mList of all trusted setups :\033[m'
+                #echo -e '1.ft-mint\n2.nft-mint\n3.ft-transfer\n4.nft-transfer\n5.ft-burn\n6.nft-burn'
+                for (( i=0; i<${#setups[@]}; i++ )); 
+                do 
+                     echo "`expr $i + 1`.${setups[$i]}"
+                done
+                echo -e $'\n''Type the number of each trusted setup seperated by a comma'
+                read -p '(E.g. type "1,2" to run the trusted setups for ft-mint & nft-mint) : ' proofs
                     proofCreation $proofs
                 exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+
+
+
